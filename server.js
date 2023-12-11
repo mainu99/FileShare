f995b1078983ef8ffdc92c535344042be3e14524
Bhargavi Borgohain <bhargaviborgohain@gmail.com>
2023-12-12 01:02:27 +0530

    initial page

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000);
